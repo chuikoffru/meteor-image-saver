3d74dbee5e187c54044bef203059843b4a4e650e
Konstantin <chuikoff.ru@gmail.com>
2015-08-06 11:04:15 +1200

    Убрал coffee scripts

--- package.js
+++ package.js
@@ -1,27 +1,20 @@
 Package.describe({
-  name: 'taehee:image-saver',
-  version: '0.0.3',
-  // Brief, one-line summary of the package.
+  name: 'chuikoff:image-saver',
+  version: '0.0.1',
   summary: 'Save images to server from url',
-  // URL to the Git repository containing the source code for this package.
-  git: 'https://github.com/kairess/meteor-image-saver',
-  // By default, Meteor will default to using README.md for documentation.
-  // To avoid submitting documentation, set this field to null.
+  git: 'https://github.com/chuikoffru/meteor-image-saver',
   documentation: 'README.md'
 });
 
 Package.onUse(function(api) {
-  api.use('coffeescript', ['client', 'server']);
-
-  api.export('ImageSaver');
-
+  api.export('ImageSaver', 'server');
   api.versionsFrom('1.1.0.2');
-  api.addFiles('image-saver.coffee', 'server');
+  api.addFiles('image-saver.js', 'server');
 });
 
 Package.onTest(function(api) {
   api.use('tinytest');
-  api.use('taehee:image-saver');
+  api.use('chuikoff:image-saver');
   api.addFiles('image-saver-tests.js');
 });
 
